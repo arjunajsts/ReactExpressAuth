c4d02146dac7dcfdd36e8d8c89b232b859aad6d5
arjuana <arjuna.jsts@gmail.com>
2026-06-14 23:21:00 +0530

    FIX Backend

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -6,27 +6,33 @@ import jwt, { type SignOptions, type VerifyOptions } from "jsonwebtoken";
 export type RefreshTokenPayload = {
   sessionId: SessionDocument["_id"];
 };
+
 export type AccessTokenPayload = {
   userId: UserDocument["_id"];
   sessionId: SessionDocument["_id"];
 };
+
 export type SignOptionsAndSecret = SignOptions & {
   secrete: string;
 };
+
 export const defaults: SignOptions = {
   audience: ["user"],
 };
+
 export const accessTokenSignOptions: SignOptionsAndSecret = {
   expiresIn: "15m",
   secrete: jWT_ACCESS_SECRET,
 };
+
 export const refreshTokenSignOptions: SignOptionsAndSecret = {
   expiresIn: "30d",
   secrete: JWT_REFRESH_SECRET,
 };
+
 export const signToken = (
   payload: AccessTokenPayload | RefreshTokenPayload,
-  options?: SignOptionsAndSecret
+  options?: SignOptionsAndSecret,
 ) => {
   const { secrete, ...signOpts } = options || accessTokenSignOptions;
   return jwt.sign(payload, secrete, {
@@ -37,18 +43,24 @@ export const signToken = (
 
 export const verifyToken = <Tpayload extends object = AccessTokenPayload>(
   token: string,
-  options?: VerifyOptions & { secrete: string }
+  options?: VerifyOptions & { secrete: string },
 ) => {
   const { secrete = jWT_ACCESS_SECRET, ...verifyOpts } = options || {};
+
+  const verifyAudience = Array.isArray(defaults.audience)
+    ? defaults.audience[0]
+    : defaults.audience;
+
   try {
     const payload = jwt.verify(token, secrete, {
-      ...defaults,
       ...verifyOpts,
-    }) as Tpayload;
+      audience: verifyAudience,
+    }) as unknown as Tpayload;
+
     return { payload };
   } catch (error: any) {
     return {
-      error: error.message,
+      error: error.message as string,
     };
   }
 };
